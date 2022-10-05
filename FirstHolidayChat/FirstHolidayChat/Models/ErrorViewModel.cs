f9a81e250afe912f57fec84de88ed86bac79957d
Deniz Ismail <Deniz.Ismail@gravitymedia.com>
2022-10-05 12:10:17 +0100

    Empty MVC web .net core project to work with

---
+++ FirstHolidayChat/FirstHolidayChat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstHolidayChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
