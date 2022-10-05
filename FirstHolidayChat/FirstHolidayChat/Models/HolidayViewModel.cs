4621045e693704d72afae7bd5e2d2c64f2d454a7
Deniz Ismail <Deniz.Ismail@gravitymedia.com>
2022-10-05 13:32:48 +0100

    View model created to get all relevant data to user, HolidayID is the only one with a changed name in the viewmodel, as it is now the HolidayRef.

---
+++ FirstHolidayChat/FirstHolidayChat/Models/HolidayViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace FirstHolidayChat.Models
+{
+    public class HolidayViewModel
+    {
+        /* City */
+        public string? HolCity { get; set; }
+        /* CityNightLife */
+        public string? NightLifeType { get; set; }
+        /* CityTemperature */
+        public string? TemperatureRating { get; set; }
+        /* Continent */
+        public string? HolContinent { get; set; }
+        /* Country */
+        public string? HolCountry { get; set; }
+        /* HolCategory */
+        public string? HolType { get; set; }
+        /* Holiday */
+        public int HolidayRef { get; set; }
+
+        /* Hotel */
+        public string? HolHotel { get; set; }
+        public int StarRating { get; set; }
+        public decimal PricePerNight { get; set; }
+
+        /* LocType */
+        public string? TerrainType { get; set; }
+
+    }
+}
