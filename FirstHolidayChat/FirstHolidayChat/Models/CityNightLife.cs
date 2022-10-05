f2b4e073ac53c51def9c366b5988b55b004a6739
Deniz Ismail <Deniz.Ismail@gravitymedia.com>
2022-10-05 13:09:50 +0100

    Entity Framework successfully generated from database with models created.

---
+++ FirstHolidayChat/FirstHolidayChat/Models/CityNightLife.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstHolidayChat.Models
+{
+    public partial class CityNightLife
+    {
+        public CityNightLife()
+        {
+            Cities = new HashSet<City>();
+        }
+
+        public int NightLifeId { get; set; }
+        public string NightLifeType { get; set; } = null!;
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
---
+++ FirstHolidayChat/FirstHolidayChat/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstHolidayChat.Models
+{
+    public partial class Country
+    {
+        public Country()
+        {
+            Cities = new HashSet<City>();
+        }
+
+        public int CountryId { get; set; }
+        public string HolCountry { get; set; } = null!;
+        public int ContinentId { get; set; }
+
+        public virtual Continent Continent { get; set; } = null!;
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
